537de33ef1fdb297bcea5ecfc218da67cb3bb065
Carlos Vivar Rios <45425937+caviri@users.noreply.github.com>
2024-05-24 14:17:34 +0200

    feat(core): Basic dt description

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # dt-example
+
+This is an example of a really basic dt execution including dataloading and interactive visualization
+
+```mermaid
+graph LR
+    HFDatabase[Huggingface Database] --> ComponentExample[Component Example]
+
+    subgraph ODTP
+        ComponentExample --> pygwalker[PygWalker]
+    end
+```
+## Tutorial
+
+1. Clone this repository
+2. Edit `dt-example.sh` with the ODTP user email 
+3. Run the bash script: `sh dt-example.sh`
---
+++ dt-example.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+ODTP_USER_EMAIL=
+DT_PATH=$(pwd)
+
+DIGITAL_TWIN_NAME=dt-example
+EXECUTION_NAME=dt-example-execution-1
+
+# Removing gitkeep
+rm ${DT_PATH}/dt-example/execution/.gitkeep
+
+# Pulling all the components and versions
+odtp new odtp-component-entry \
+--name odtp-component-example \
+--component-version 0.1.1 \
+--repository https://github.com/odtp-org/odtp-component-example 
+
+odtp new odtp-component-entry \
+--name odtp-pygwalker \
+--component-version v0.1.0 \
+--repository https://github.com/odtp-org/odtp-postgis-dataloader
+
+# Creating new digital twin
+odtp new digital-twin-entry \
+--user-email ${ODTP_USER_EMAIL}  \
+--name ${DIGITAL_TWIN_NAME}
+
+# Creating new execution
+odtp new execution-entry \
+--name ${EXECUTION_NAME} \
+--digital-twin-name ${DIGITAL_TWIN_NAME} \
+--component-tags odtp-component-example:v0.1.1,odtp-pygwalker:v0.1.0 \
+--parameter-files ${DT_PATH}/dt-example/001.parameters, \
+--ports ,8502:8501
+
+# Preparing execution
+odtp execution prepare \
+--execution-name ${EXECUTION_NAME} \
+--project-path ${DT_PATH}/dt-example/execution
+
+# Running execution
+odtp execution run \
+--execution-name ${EXECUTION_NAME} \
+--project-path ${DT_PATH}/dt-example/execution

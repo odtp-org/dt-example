310acc079f43a13c36626c405db2909b991e874a
Dotfiles-Home <Dotfiles@home.ch>
2024-12-20 15:44:11 +0100

    feat: adapt script to odtp v0.5.0

--- dt-example.sh
+++ dt-example.sh
@@ -22,17 +22,11 @@ odtp new digital-twin-entry \
 --user-email ${ODTP_USER_EMAIL} \
 --name ${DIGITAL_TWIN_NAME}
 
-# Creating new workflow
-odtp new workflow-entry \
---dt-name ${DIGITAL_TWIN_NAME} \
---name ${WORKFLOW_NAME} \
---component-tags odtp-component-example:v0.1.8,odtp-pygwalker:v0.1.6a
-
 # Creating new execution
 odtp new execution-entry \
 --name ${EXECUTION_NAME} \
---component-tags odtp-component-example:v0.1.8,odtp-pygwalker:v0.1.6a \
---parameter-files ${DT_PATH}/dt-example/001.parameters, \
+--digital-twin-name ${DIGITAL_TWIN_NAME} \
+--component-tags odtp-component-example:v0.1.8,odtp-pygwalker:v0.1.6 \
 --ports ,8502:8501
 
 # Preparing execution
